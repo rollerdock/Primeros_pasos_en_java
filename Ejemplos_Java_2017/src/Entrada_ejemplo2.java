b544c7f21d4e5a02c3f60ff2e4fe89b75b678706
jaspl <jaspl@192.168.1.18>
2017-08-23 21:23:08 +0200

    javax.swing.*; Ejemplos con JOptionPane.showInputDialog Entrada de datos con popup gráfico.

---
+++ Ejemplos_Java_2017/src/Entrada_ejemplo2.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+public class Entrada_ejemplo2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ String nombre_usuario=JOptionPane.showInputDialog("Introduce tu nombre");//input 
+ String edad=JOptionPane.showInputDialog("Introduce la edad por favor");
+ System.out.println("Hola " + nombre_usuario + "Tienes " + edad + " Años");
+
+ 
+	}
+
+}
