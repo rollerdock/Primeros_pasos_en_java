2a20dfc3b5f6433db7b10a2459dc2b88fb989b7e
jaspl <jaspl@192.168.1.18>
2017-08-23 22:39:04 +0200

    Con la clase Integer (estatica) y el método parseInt(string)  y convertimos un string en un int

--- Ejemplos_Java_2017/src/Entrada_ejemplo2.java
+++ Ejemplos_Java_2017/src/Entrada_ejemplo2.java
@@ -5,7 +5,12 @@ public class Entrada_ejemplo2 {
 		// TODO Auto-generated method stub
  String nombre_usuario=JOptionPane.showInputDialog("Introduce tu nombre");//input 
  String edad=JOptionPane.showInputDialog("Introduce la edad por favor");
- System.out.println("Hola " + nombre_usuario + "Tienes " + edad + " Años");
+ int edad_usuario=Integer.parseInt(edad);
+ 
+ //con la clase Integer (estatica) y el método parseInt(string) 
+ //convertimos un string en un int
+ 
+ System.out.println("Hola " + nombre_usuario + ", El año que viene tendrás " + (edad_usuario+1) + " Años");
 
  
 	}
